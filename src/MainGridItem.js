3486afcf3ee31b9879c861c6ca1fa912652f5f26
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-20 00:35:11 +0200

    updated MainGridItem

--- src/MainGridItem.js
+++ src/MainGridItem.js
@@ -1,127 +1,73 @@
 import {
     Container,
+    Card,
     Typography,
+    Button,
+    CardMedia,
     Grid
 } from '@material-ui/core';
-
-import GridItem from './GridItem';
-
-import {
-    ThumbUp,
-    Build,
-    VerifiedUser,
-    Face,
-    VideoLibrary,
-    CardMembership,
-    Public,
-    School,
-    Loyalty
-} from '@material-ui/icons';
 
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
 
+    cardMedia: {
+        height: 300
+    },
+    mainItem: {
+        padding: theme.spacing(8),
+        textAlign: "left"
+    },
     mainContainer: {
-        paddingLeft: 40,
-        paddingRight: 40,
         paddingTop: 120,
-        paddingBottom: 120
+        paddingBottom: 120,
+        backgroundColor: "darkorange"
+    },
+    imageCard: {
+        margin: 40
     }
 
 }));
 
-export default function ProductInformation() {
+export default function MainGridItem() {
 
-    const { mainContainer } = useStyles();
+    const { cardMedia, mainItem, mainContainer, imageCard } = useStyles();
 
     return (
 
         <Container className={mainContainer}>
+            <Card>
 
-            <Typography variant="h2">
-                What You'll Get
-            </Typography>
+                <Grid container>
 
-            <Grid container>
+                    <Grid item md={6} className={mainItem}>
 
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="200 courses"
-                        description="20-60 hours of new courses every month"
-                        icon={<ThumbUp />}
-                    />
+                        <Typography variant="h2">
+                            Unlimited Subscription
+                        </Typography>
+
+                        <Typography>
+                            2,000 hours of online courses.
+                        </Typography>
+
+                        <Button color="primary" variant="contained">
+                            Sign Up
+                        </Button>
+
+                    </Grid>
+
+                    <Grid item md={6}>
+
+                        <Card className={imageCard}>
+                            <CardMedia className={cardMedia} image="https://cdn.pixabay.com/photo/2020/12/18/16/56/laptop-5842509_1280.jpg"
+                            />
+                        </Card>
+
+                    </Grid>
+
                 </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Build projects"
-                        description="Project-based curriculum to build your portfolio"
-                        icon={<Build />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Earn certificates"
-                        description="Graduation certificate for every course"
-                        icon={<VerifiedUser />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="All levels"
-                        description="Courses for absolute beginners and the experienced"
-                        icon={<Face />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Trusted curricula"
-                        description="Efficient lectures with step by step explanations"
-                        icon={<VideoLibrary />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Industry standard"
-                        description="10 years of award-winning course delivery"
-                        icon={<CardMembership />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Worldwide community"
-                        description="1 million+ students in 186 countries"
-                        icon={<Public />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Accessible software"
-                        description="Learn with free tools and affordable courses"
-                        icon={<School />}
-                    />
-                </Grid>
-
-                <Grid item lg={4} md={12} sm={12} xs={12}>
-                    <GridItem
-                        title="Only $19 a month"
-                        description="Yearly and lifetime options available"
-                        icon={<Loyalty />}
-                    />
-                </Grid>
-
-
-            </Grid>
-
+            </Card>
         </Container>
 
     );
-
 }
