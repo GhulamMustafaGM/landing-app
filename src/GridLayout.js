81dfdbd1df0fbd7d11d65654aa2b9823dd46fd28
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-19 23:58:15 +0200

    created GridItem and GridLayout pages

---
+++ src/GridLayout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {
+    Grid
+} from '@material-ui/core';
+
+import MainGridItem from './MainGridItem';
+import ProductInformation from './ProductInformation';
+
+export default function GridLayout() {
+
+    return (
+
+        <Grid container>
+
+            <Grid item>
+
+                <MainGridItem />
+
+            </Grid>
+
+            <Grid item>
+
+                <ProductInformation />
+
+            </Grid>
+        </Grid>
+
+    );
+}
